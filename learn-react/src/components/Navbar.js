7045b24361ffcdb722ebb23f65be6ea4615a5688
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-11 17:24:37 -0500

    Styling Navbar

--- learn-react/src/components/Navbar.js
+++ learn-react/src/components/Navbar.js
@@ -1,7 +1,12 @@
+import logo from "../images/react-icon-small.png"
+
 export default function Navbar() {
     return (
-        <>
-            <h1>This is Navbar Component</h1>
-        </>
+        <nav className="navbar">
+            <img alt="React Logo" src={logo} />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+
+        </nav>
     )
 }
